641fc588eec15d95c53b21c5b7ef6218155d4686
Girish Gokul <girishg1192@gmail.com>
2016-02-29 19:26:52 -0500

    Using Audiotrack

--- app/src/main/java/edu/buffalo/record/MainActivity.java
+++ app/src/main/java/edu/buffalo/record/MainActivity.java
@@ -1,11 +1,16 @@
 package edu.buffalo.record;
 
 import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,16 +25,20 @@ public class MainActivity extends AppCompatActivity {
     private static String mFileName = null;
     private static boolean firstPlay = true;
     private static boolean firstRecord = true;
+    private final int STREAM_PLAY = 44100;
 
     private RecordButton mRecordButton = null;
     private MediaRecorder mRecorder = null;
 
     private PlayButton mPlayButton = null;
     private MediaPlayer mPlayer = null;
+    private PlayStuff mPlayStuff = null;
 
     public void recordStream(boolean startRecording){
+        Log.v(LOG_TAG, "RecordStream" + startRecording);
         if(startRecording){
             if(firstRecord) {
+                Log.v(LOG_TAG, "Create MediaRecorder");
                 mRecorder = new MediaRecorder();
                 firstRecord = false;
             }
@@ -40,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
                 mRecorder.setOutputFile(mFileName);
                 mRecorder.prepare();
+                Log.v(LOG_TAG, "Record start");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -57,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 firstPlay = false;
             }
             try {
+                Log.v(LOG_TAG, "file name" + mFileName);
                 mPlayer.setDataSource(mFileName);
                 mPlayer.prepare();
             } catch (IOException e) {
@@ -65,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
             mPlayer.start();
         }
         else{
+            Log.v(LOG_TAG, "Stop playback");
             mPlayer.stop();
             mPlayer.reset();
         }
@@ -111,6 +123,39 @@ public class MainActivity extends AppCompatActivity {
             setOnClickListener(clicker);
         }
     }
+    class PlayStuff extends Button {
+        boolean mStart = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                playStream(mStart);
+                if (mStart) {
+                    setText("Stop Playback");
+                } else {
+                    setText("Start Playback");
+                }
+                mStart = !mStart;
+            }
+        };
+        public PlayStuff(Context ctx) {
+            super(ctx);
+            setText("Start Loopback");
+            setOnClickListener(clicker);
+        }
+    }
+    private class LoopbackThread extends Thread{
+        @Override
+        public void run(){
+            int bufferSize = AudioTrack.getMinBufferSize( 44100,
+                    AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
+            AudioTrack play= new AudioTrack(AudioManager.STREAM_MUSIC, STREAM_PLAY,
+                    AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
+            short[] buffer = new short[bufferSize];
+            AudioRecord record = new AudioRecord(MediaRecorder.AudioSource.MIC, 44100, AudioFormat.CHANNEL_IN_STEREO,
+                    AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+            }
+
+    }
     public MainActivity(){
         mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
         mFileName += "/file.amr";
@@ -120,11 +165,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout ll = new LinearLayout(this);
         /*
         mRecordButton = (RecordButton)findViewById(R.id.recordButton);
         mPlayButton = (PlayButton) findViewById(R.id.playButton);
-        */
-        LinearLayout ll = new LinearLayout(this);
+        *
         mRecordButton = new RecordButton(this);
         ll.addView(mRecordButton,
                 new LinearLayout.LayoutParams(
@@ -133,6 +178,14 @@ public class MainActivity extends AppCompatActivity {
                         0));
         mPlayButton = new PlayButton(this);
         ll.addView(mPlayButton,
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        0));
+                LinearLayout ll = new LinearLayout(this);
+                */
+        mPlayStuff = new PlayStuff(this);
+        ll.addView(mPlayStuff,
                 new LinearLayout.LayoutParams(
                         ViewGroup.LayoutParams.WRAP_CONTENT,
                         ViewGroup.LayoutParams.WRAP_CONTENT,
